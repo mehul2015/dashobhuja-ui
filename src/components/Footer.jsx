1d2c3e3457dff1eebe67f7c97068136096a3afab
mehul2015 <mehuldev2015@gmail.com>
2022-08-21 10:09:26 +0530

    Setup + first draft

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,57 +1,7 @@
-import * as React from 'react';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import FolderIcon from '@mui/icons-material/Folder';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import React from 'react';
 
-import styled from 'styled-components';
+const Footer = () => {
+  return <div>Footer</div>;
+};
 
-const Container = styled.div`
-  height: 10vh;
-  width: 100vw;
-  background-color: black;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-export default function Footer() {
-  const [value, setValue] = React.useState('recents');
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <Container>
-      <BottomNavigation
-        sx={{ width: 2000 }}
-        value={value}
-        onChange={handleChange}
-      >
-        <BottomNavigationAction
-          label='Recents'
-          value='recents'
-          icon={<RestoreIcon />}
-        />
-        <BottomNavigationAction
-          label='Favorites'
-          value='favorites'
-          icon={<FavoriteIcon />}
-        />
-        <BottomNavigationAction
-          label='Nearby'
-          value='nearby'
-          icon={<LocationOnIcon />}
-        />
-        <BottomNavigationAction
-          label='Folder'
-          value='folder'
-          icon={<FolderIcon />}
-        />
-      </BottomNavigation>
-    </Container>
-  );
-}
+export default Footer;
